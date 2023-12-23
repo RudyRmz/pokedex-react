c142af945741a2add4efab10656a6dd493b1deb1
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2023-12-23 11:45:41 -0600

    se agregaron estilos y efectos a las cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -8,21 +8,30 @@ export default function PokemonCard({ name, url }) {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        //console.log(data);
         // SetPokemon(data.sprites.front_default);
         SetPokemon(data);
       });
   }, []);
 
   return (
-    <article>
+    <article
+      className={clsx(
+        " bg-green-500",
+        "h-64 w-50",
+        "flex flex-col items-center p-4 gap-1",
+        "rounded-md shadow-lg",
+        "hover:-translate-y-1 hover:opacity-85 cursor-pointer"
+      )}
+    >
       {/* <img src={pokemon} alt="" className={clsx("w-[150px]")} /> */}
       <img
-        src={pokemon?.sprites?.other?.dream_world?.front_default}
+        src={pokemon?.sprites?.front_default}
         alt=""
-        className={clsx("w-[125px] max-w[150px]")}
+        className={clsx("w-[150px] max-w[150px] bg-white", "rounded-md")}
       />
-      <h3>{name.toUpperCase()}</h3>
+      <h3 className={clsx("font-bold tracking-wide")}>{name.toUpperCase()}</h3>
+      <h2 className={clsx("font-medium")}>Type:</h2>
     </article>
   );
 }
